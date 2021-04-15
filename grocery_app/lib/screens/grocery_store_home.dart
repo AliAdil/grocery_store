b68f2a94df5fe8d1f15d85b29cc3048292c4ac33
Ali Adil <aliadilg@gmail.com>
2021-04-15 22:51:52 +0500

    update main screen

--- grocery_app/lib/screens/grocery_store_home.dart
+++ grocery_app/lib/screens/grocery_store_home.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
 
+const _backgroudColor = Color(0XFFF6F5F2);
+
 class GroceryStoreHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          _AppBarGrocery(),
+        ],
+      ),
+    );
+  }
+}
+
+class _AppBarGrocery extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: child,
+      height: kToolbarHeight,
+      color: _backgroudColor,
+      child: Row(
+        children: [
+          BackButton(
+            color: Colors.black,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Expanded(
+              child: Text(
+            'Fruit and vegetables',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          )),
+          IconButton(
+              icon: Icon(
+                Icons.settings,
+              ),
+              onPressed: () => null)
+        ],
+      ),
     );
   }
 }
